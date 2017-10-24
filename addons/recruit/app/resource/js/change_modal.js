ec51c730f598febc7778c9eea5281a96cc4afbad
1421514791@qq.com <1421514791@qq.com>
2017-10-24 16:12:40 +0800

    111

--- addons/recruit/app/resource/js/change_modal.js
+++ addons/recruit/app/resource/js/change_modal.js
@@ -14,15 +14,32 @@ $(function () {
 
 
     //选择使用模板
-    $("body").on("click",".modal_tijiao",function () {
-        var list_modal=$(".swiper-slide");
-        list_modal.each(function () {
-            if($(this).find(".biankuang").length>0){
-                var i=$(this).attr("data-id");
-                window.location.href="resume_preview"+i+".html";
+    // $("body").on("click",".modal_tijiao",function () {
+    //     var list_modal=$(".swiper-slide");
+    //     list_modal.each(function () {
+    //         if($(this).find(".biankuang").length>0){
+    //             var i=$(this).attr("data-id");
+    //             window.location.href="resume_preview"+i+".html";
+    //         }
+    //     })
+    //
+    // })
+
+
+    $(".tijiao").click(function () {
+        var template_id = $(".biankuang").parent().parent().attr('data-id');
+        $.ajax({
+            type:"post",
+            data:{
+                template_id:template_id
+            },
+            success:function (data) {
+                var data =JSON.parse(data);
+                if(data.status==1){
+                    location = location;
+                }
             }
         })
-
-    })
+    });
 
 })
