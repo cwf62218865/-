29838a18fb066104d233d162d3885f19549c2eed
“用cwf <308466155@qq.com>
2017-10-31 10:48:50 +0800

    首页

---
+++ addons/recruit/app/resource/js/index.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Administrator on 2017/10/30 0030.
+ */
+
+    var index_menu=index_menu;
+    var leftmenus="";
+
+    for(var i=0 ;i<index_menu.length; i++){
+
+        leftmenus+='<div class="index_menu">';
+        leftmenus+='<span class="index_menu_title">'+index_menu[i].title+'</span>';
+        for(var k=0 ; k<index_menu[i].name.length; k ++){
+            leftmenus+='<span>'+index_menu[i].name[k]+'</span>';
+        }
+        leftmenus+='<svg class="icon">'+
+            '<use xlink:href="#icon-more"></use>'+
+            '</svg></div>';
+    }
+
+    $(".index_menus").html(leftmenus);
+
+
+    $(".resume_job_title").eq(0).on("click",function(){
+        $(".resume_job_title").eq(0).addClass("relative remengongsi").removeClass("color999");
+        $(".resume_job_title").eq(1).removeClass("relative remengongsi").addClass("color999");
+        $(".resume_list").eq(0).show();
+        $(".resume_list").eq(1).hide();
+    });
+    $(".resume_job_title").eq(1).on("click",function(){
+        $(".resume_job_title").eq(1).addClass("relative remengongsi").removeClass("color999");
+        $(".resume_job_title").eq(0).removeClass("relative remengongsi").addClass("color999");
+        $(".resume_list").eq(1).show();
+        $(".resume_list").eq(0).hide();
+    });
