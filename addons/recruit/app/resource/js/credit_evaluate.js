ba579adf2ace3c337f52aca9bcc81968798870cf
“zhangjing <2370401878@qq.com>
2017-11-03 11:09:02 +0800

    周三  11-3 收到评价、待评价

---
+++ addons/recruit/app/resource/js/credit_evaluate.js
@@ -0,0 +1,365 @@
+$(function () {
+    $(".list_item_btn ").click(function () {
+        $(".list_item_btn ").removeClass("select");
+        $(this).addClass("select");
+        var i=$(this).index();
+        $(".lists").hide();
+        $(".lists").eq(i).show();
+    })
+})
+
+//我的评价的展开和收起
+$("body").on("click",".zhankai",function () {
+    $(this).closest(".list_item1").find(".my_evaluate").show();
+    $(this).html("收起我的评价");
+    $(this).removeClass("zhankai");
+    $(this).addClass("shouqi");
+});
+$("body").on("click",".shouqi",function () {
+    $(this).closest(".list_item1").find(".my_evaluate").hide();
+    $(this).html("展开我的评价");
+    $(this).removeClass("shouqi");
+    $(this).addClass("zhankai");
+});
+
+
+//省略号显示
+var pj=$(".pingjia_neir").html();
+if(pj.length>60){
+    $(".pingjia_neir").html(pj.substring(0,60)+"......");
+}
+var htmls="收起<svg class='icon' aria-hidden='true'><use xlink:href='#icon-shangjiantou'></use></svg>";
+var htmls1="显示全部<svg class='icon' aria-hidden='true'><use xlink:href='#icon-xiajiantou'></use></svg>";
+$("body").on("click",".xianshi_ico",function () {
+    var pj1=$(this).prev().html();
+    if(pj1.length<67){
+        $(this).prev().html(pj);
+        $(this).html(htmls);
+    }else{
+        $(this).prev().html(pj.substring(0,60)+"......");
+        $(this).html(htmls1);
+    }
+})
+
+//勾选匿名
+$(".checkbox").on("click",function () {
+    var right="<svg class='icon iconfont color1aa ico_right'><use  xlink:href='#icon-zhengque1'></use></svg>";
+    var con=$(this).html();
+    if(con){
+        $(this).html("");
+    }else{
+        $(this).html(right);
+    }
+})
+
+
+//初始化弹窗
+$("body").on("click",".jubaopl,.jubaoms",function () {
+    $("#jubaobox").show();
+    $("#jubaobox .options").html("");
+    $(".detailcon").val("");
+    $(".ico_niming .checkbox").html("");
+
+})
+
+//举报评论
+$("body").on("click",".jubaopl",function () {
+    var data_id=$(this).closest(".list_item1").attr("data-id");
+    $("#jubaobox").attr("data-id",data_id);
+    var reason=["说话太随意","面试不正规"];
+    $("#jubaobox .title_content").html("举报该评论");
+    $("#jubaobox .tijiao1").removeClass("jubaogs");
+    $("#jubaobox .tijiao1").addClass("jubaopinglun");
+    for(var i=0;i<reason.length;i++){
+        $("#jubaobox .options").append('<div class="select-option" style="width: 330px"><span>'+reason[i]+'</span></div>');
+    }
+})
+
+//举报公司
+$("body").on("click",".jubaoms",function () {
+    var data_id=$(this).closest(".list_item1").attr("data-id");
+    $("#jubaobox").attr("data-id",data_id);
+    var reason=["培训机构","工资不真实"];
+    $("#jubaobox .title_content").html("举报该公司");
+    $("#jubaobox .tijiao1").removeClass("jubaopinglun");
+    $("#jubaobox .tijiao1").addClass("jubaogs");
+    for(var i=0;i<reason.length;i++){
+        $("#jubaobox .options").append('<div class="select-option" style="width: 330px"><span>'+reason[i]+'</span></div>');
+    }
+})
+//举报评论提交
+$("body").on("click",".jubaopinglun",function () {
+    var select=$(".jubaoreason").val();
+    var detailcon=$(".detailcon").val();
+    var niming;//1为匿名，0为不匿名
+    var ico=$(this).closest("#jubaobox").find(".ico_niming .checkbox");
+    if(ico.html()){
+        niming=1;
+    }else{
+        niming=0;
+    }
+
+    console.log(select+"。。。。。"+detailcon+"。。。。。。"+niming);
+
+
+    $.ajax({
+        url:"",
+        type:"post",
+        data:{
+            select:select,
+            detailcon:detailcon,
+            niming:niming
+        },
+        success:function (data) {
+            if(data.status==1){
+                window.location.href="";
+            }else{
+                console.log(data.content);
+            }
+        }
+    })
+
+})
+
+//举报公司提交
+$("body").on("click",".jubaogs",function () {
+    var select=$(".jubaoreason").val();
+    var detailcon=$(".detailcon").val();
+    var niming;//1为匿名，0为不匿名
+    var ico=$(this).closest("#jubaobox").find(".ico_niming .checkbox");
+    if(ico.html()){
+        niming=1;
+    }else{
+        niming=0;
+    }
+    console.log(select+"。。。。。"+detailcon+"。。。。。。"+niming);
+
+
+    $.ajax({
+        url:"",
+        type:"post",
+        data:{
+            select:select,
+            detailcon:detailcon,
+            niming:niming
+        },
+        success:function (data) {
+            if(data.status==1){
+                window.location.href="";
+            }else{
+                console.log(data.content);
+            }
+        }
+    })
+
+})
+
+//好评中评差评的勾选，只能选1个
+$(".one_pj .checkbox").click(function () {
+    var _this=$(this).closest(".pjxingxing");
+    var index=_this.find(".one_pj").index();
+    _this.find(".checkbox").html("");
+    $(this).html('<svg class="icon iconfont color1aa ico_right"><use  xlink:href="#icon-zhengque1"></use> </svg>');
+    _this.attr("value",index);
+})
+
+
+//待评价板块评价提交
+$("body").on("click",".pjtjbtn",function () {
+    var _this=$(this).closest(".item_conpj");
+    var num1=_this.find(".xinxi").attr("value");
+    var num2=_this.find(".huanjing").attr("value");
+    var num3=_this.find(".hrmianshi").attr("value");
+    var pingfen=[num1,num2,num3];
+    var biaoqian=[];
+    var detail=_this.find(".plarea").val();
+    var pingjia=_this.find(".pjxingxing").attr("value");//好评0，中评1，差评2
+    var niming=0;
+    var biaoqianlist=_this.find(".biaoqianxuanze .one_se");
+    for(var i=0;i<biaoqianlist.length;i++){
+        biaoqian.push(biaoqianlist.eq(i).text());
+    }
+    var ico=_this.find(".ico_niming  .checkbox");
+    if(ico.html()){
+        niming=1;
+    }else{
+        niming=0;
+    }
+    console.log(num1+"<br>"+num2+"<br>"+num3+"<br>"+biaoqian[0]+"<br>"+biaoqian[1]+"<br>"+biaoqian[2]+"<br>"+detail+"<br>"+pingjia+"<br>"+niming)
+
+    $.ajax({
+        url:"",
+        type:"post",
+        data:{
+            pingfen:pingfen,
+            biaoqian:biaoqian,
+            detail:detail,
+            pingjia:pingjia,
+            niming:niming
+        },
+        success:function (data) {
+            if(data.status==1){
+                window.location.href="";
+            }else{
+                console.log(data.content);
+            }
+        }
+    })
+
+})
+
+
+//未参加
+$("body").on("click",".weicanjia",function () {
+    $("#weicanjiabox").show();
+    $("#weicanjiabox").find(".tijiao1").addClass("weicanjiatj");
+})
+$("body").on("click",".weicanjiatj",function () {
+    var lists=$(".xuanxiang .itemsxx");
+    var reason=[];
+    var zidy=$(this).closest(".weicanjiabox").find(".weicanj").val();
+    lists.each(function () {
+        if($(this).find(".checkbox").html()!=""){
+            reason.push($(this).find(".sss").html());
+        }
+    })
+    $.ajax({
+        url:"",
+        type:"post",
+        data:{
+            reason:reason,
+            zidy:zidy
+        },
+        success:function (data) {
+            if(data.status==1){
+                window.location.href="";
+            }else{
+                console.log(data.content);
+            }
+        }
+    })
+
+
+})
+
+//弹框关闭
+$(".modalclose,.quxiao").click(function () {
+    $(this).closest(".modalbox").hide();
+})
+
+//更多标签
+$("body").on("click",".gengduo",function () {
+    $(this).html('收起<svg class="icon"><use xlink:href="#icon-shangjiantou"></use></svg>');
+    $(".morebiaoqian").show();
+    $(this).removeClass("gengduo");
+    $(this).addClass("shouqi");
+})
+$("body").on("click",".shouqi",function () {
+    $(this).html('更多标签<svg class="icon"><use xlink:href="#icon-xiajiantou"></use></svg>');
+    $(".morebiaoqian").hide();
+    $(this).removeClass("shouqi");
+    $(this).addClass("gengduo");
+})
+
+//评论星星
+var finalnum = 0;
+var tempnum =0;
+var lis1 = $(".xinxi .xingxing");
+var lis2 = $(".huanjing .xingxing");
+var lis3 = $(".hrmianshi .xingxing");
+
+//    $(".xingxing").on("mouseover",function () {
+//        $(this).parent().find("use").attr("xlink:href","#icon-pingfenbanfen");
+//        $(this).prevAll().find("use").attr("xlink:href","#icon-pingfen");
+//        $(this).find("use").attr("xlink:href","#icon-pingfen");
+//        showxingxing(tempnum);
+//    });
+//
+//    function showxingxing(num) {
+//
+//    }
+var arr=[lis1,lis2,lis3];
+//    num:传入点亮星星的个数
+//    finalnum:最终点亮星星的个数
+//    tempnum:一个中间值
+$.each(arr,function(i,value){
+    for(var i = 1; i <= value.length; i++) {
+        value.eq(i-1).index(i);
+        value.mouseover(function(){
+//                fnShow($(this).index());
+            tempnum = $(this).index();
+        })
+        value.mouseleave(function(){
+//                fnShow(0);
+        })
+        value.click(function(){
+            tempnum = $(this).index();
+            var num=fnShow($(this).index());
+            $(this).closest(".one_xing ").attr("value",num);
+
+        })
+    }
+    function fnShow(num) {
+        finalnum = num || tempnum;
+        for(var i = 0; i < value.length; i++) {
+            value.eq(i).html(i < finalnum ? '<use  xlink:href="#icon-pingfen"></use>' : '<use  xlink:href="#icon-pingfenbanfen"></use>');
+        }
+        return finalnum;
+    }
+
+});
+
+
+//添加评论标签
+
+$("body").on("click",".onebq",function () {
+    var len=$(".one_se").length;
+    var bq=$(this).html();
+    var flag=$(this).attr("flag");
+    if(flag==0 && len<5){
+        $(".biaoqianxuanze").append('<span class="one_se">' +bq +'<svg class="icon shnchuico" style=""><use  xlink:href="#icon-shan"></use></svg></span>')
+        $(this).attr("flag",1);}
+    if(len==5){
+        alert("最多只能添加5个标签！")
+    }
+})
+$("body").on("click",".btntj",function () {
+    var len=$(".one_se").length;
+    var zbq=$(".bqzidingyi").val();
+    if(zbq && len<5){
+        $(".biaoqianxuanze").append('<span class="one_se">' +zbq +'<svg class="icon shnchuico" style=""><use  xlink:href="#icon-shan"></use></svg></span>')
+        var zbq=$(".bqzidingyi").val("");
+    }
+    if(len==5){
+        alert("最多只能添加5个标签！")
+        var zbq=$(".bqzidingyi").val("");
+    }
+})
+//删除标签
+$("body").on("click",".shnchuico",function () {
+    var item=$(this).closest(".one_se");
+    item.remove();
+    $(".onebq").each(function () {
+        if(item.text()==$(this).html()){
+            $(this).attr("flag",0);
+        }
+    })
+})
+
+
+
+//下拉选择
+$(".general-select input").on("click",function(){
+    var _this=$(this);
+    if(_this.closest(".general-select").next().height()=="0"){
+        _this.closest(".general-select").next().css("height","auto");
+    }else {
+        _this.closest(".general-select").next().css("height","0px");
+    }
+
+});
+$("#jubaobox").on("mousedown",".select-option",function () {
+    var con=$(this).find("span").html();
+    $(this).closest(".general-select").find("input").val(con);
+    $(this).closest(".options").css("height","0px");
+});
