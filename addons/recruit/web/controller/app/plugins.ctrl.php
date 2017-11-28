7907f851e1be9d54b0b2a0fbe4795ba7811d8e0c
1421514791@qq.com <1421514791@qq.com>
2017-11-28 13:52:41 +0800

    Merge branch 'master' of github.com:cwf62218865/yingjieseng
    # Conflicts:
    #	addons/recruit/app/view/pc/person/send_resume.html

--- addons/recruit/web/controller/app/plugins.ctrl.php
+++
@@ -1,8 +0,0 @@
-<?php 
-defined('IN_IA') or exit('Access Denied');
-$ops = array('list');
-$op = in_array($op, $ops) ? $op : 'list';
-
-if ($op == 'list') {
-	include wl_template('app123/plugins_list');
-}
---
+++ addons/recruit/web/controller/company/detail.ctrl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/11/28 0028
+ * Time: 13:46
+ */
+
+include wl_template('company/detail');exit();
