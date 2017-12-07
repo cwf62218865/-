80513fba4e2700a0590c67d671aa4447dca0a30f
1421514791@qq.com <1421514791@qq.com>
2017-12-07 17:56:24 +0800

    Merge branch 'master' of github.com:cwf62218865/yingjieseng
    # Conflicts:
    #	addons/recruit/app/view/pc/person/person_collection.html

--- addons/recruit/app/resource/js/news_detail.js
+++ addons/recruit/app/resource/js/news_detail.js
@@ -30,10 +30,15 @@ $(function () {
     //回复评论的点击效果
     $(".huifu_btn").click(function () {
         $(".text_area_pl1,.fabupjbtn1").remove();
+        var id = $(this).parent().parent().parent().attr("data-id");
+        var pl_user = $(this).parent().parent().parent().find(".pl_user").html();
         var _this=$(this).closest(".pinglun_item");
         var html='<div class="text_area_pl1"><textarea class="shuru_text1" placeholder="请输入评论"></textarea></div><div class="fabupjbtn1">提交回复</div>';
         if(_this.find(".text_area_pl1").length==0){
             _this.append(html);
+            $(".fabupjbtn1").attr("data-commentid",id);
+            $(".fabupjbtn1").attr("data-id",$(".fabupjbtn").attr("data-id"));
+            $(".fabupjbtn1").attr("data-pl_user",pl_user);
         }
 
     })
@@ -47,42 +52,47 @@ $(function () {
         $(".text_area_pl").append("<style>.text_area_pl::before{     border-bottom: 10px solid #eeeeee; }</style>");
     })
     $(".fabupjbtn").click(function () {
+
+        var headimgurl = $(".touxiang").find("img").attr("src");
         var pj_content=$(".shuru_text").val();
         var user="";
         if(pj_content==""){
             hint("error","请填写你的评论");
             return
         }else{
-
-            html='<div class="pinglun_item">\n' +
-                '                <div class="touxiang_pl">\n' +
-                '                    <img src="http://localhost/addons/recruit/app/resource/images/resume_tx1.png" style="width: 100%;">\n' +
-                '                </div>\n' +
-                '                <div class="pl_user">我的评论</div>\n' +
-                '                <div class="pinglunneir">'+pj_content+'</div>\n' +
-                '                <div class="pl_status">\n' +
-                '                    <label class="time_pl">11小时前</label>\n' +
-                '                    \n' +
-                '                </div>';
-            $(".pinglu_title").after(html);
-            $(".shuru_text").val("");
             $.ajax({
-                url:"",
+                url:"/app/index.php?c=site&a=entry&m=recruit&do=member&ac=index&op=comment_news",
                 type:"post",
                 data:{
+                    id:$(this).attr("data-id"),
                     pj_content:pj_content,
-                    user:user
+                    comment_id:0
                 },
                 success:function(data){
                     if(data){
                         var data=JSON.parse(data);
                         if(data.status==1){
                             hint("success","评论成功");
-                            location=location;
 
-                        }else{
+                            html='<div class="pinglun_item">\n' +
+                                '                <div class="touxiang_pl">\n' +
+                                '                    <img src="'+headimgurl+'" style="width: 100%;">\n' +
+                                '                </div>\n' +
+                                '                <div class="pl_user">我的评论</div>\n' +
+                                '                <div class="pinglunneir">'+pj_content+'</div>\n' +
+                                '                <div class="pl_status">\n' +
+                                '                    <label class="time_pl">1分钟前</label>\n' +
+                                '                    \n' +
+                                '                </div>';
+                            $(".pinglu_title").after(html);
+                            $(".shuru_text").val("");
+                            // location=location;
+                        }else if(data.status==2){
                             hint("error",data.content);
+                        }else if(data.status==3){
+                            $("#small_modalbox").show();
                         }
+
                     }
                 }
             })
@@ -91,45 +101,50 @@ $(function () {
 
     //回复评论-提交
     $("body").on("click",".fabupjbtn1",function () {
+        var _this = $(this);
+        var headimgurl = $(".touxiang").find("img").attr("src");
+        var fullname = $(".user_name").html();
+        var pl_user = $(this).attr("data-pl_user");
+        var comment_id =$(this).attr("data-commentid");
         var plhf=$(this).prev(".text_area_pl1").find(".shuru_text1").val();
         var user="";
+
         if(plhf==""){
             hint("error","请输入内容");
             return
         }else{
-
-            html='<div class="pinglun_item">\n' +
-                '                <div class="touxiang_pl">\n' +
-                '                    <img src="http://localhost/addons/recruit/app/resource/images/resume_tx1.png" style="width: 100%;">\n' +
-                '                </div>\n' +
-                '                <div class="pl_user">张静</div>\n' +
-                '                <div class="pinglunneir"><label class="beizhu_pl">[回复黄振宇]</label>'+plhf+'</div>\n' +
-                '                <div class="pl_status">\n' +
-                '                    <label class="time_pl">11小时前</label>\n' +
-                '                    <label class="ico_plbtn">\n' +
-                '                        <span class="jubao_btn">举报</span>\n' +
-                '                        <span class="huifu_btn">回复</span>\n' +
-                '                    </label>\n' +
-                '                </div>\n' +
-                '            </div>';
-
-            $(".text_area_pl1").hide();
-            $(".fabupjbtn1").hide();
-            $(this).closest(".pinglun_item").after(html);
-
             $.ajax({
-                url:"",
+                url:"/app/index.php?c=site&a=entry&m=recruit&do=member&ac=index&op=comment_news",
                 type:"post",
                 data:{
-                    plhf:plhf,
-                    user:user
+                    id:$(this).attr("data-id"),
+                    pj_content:plhf,
+                    comment_id:comment_id
                 },
                 success:function(data){
                     if(data){
                         var data=JSON.parse(data);
                         if(data.status==1){
+                            html='<div class="pinglun_item">\n' +
+                                '                <div class="touxiang_pl">\n' +
+                                '                    <img src="'+headimgurl+'" style="width: 100%;">\n' +
+                                '                </div>\n' +
+                                '                <div class="pl_user">'+fullname+'</div>\n' +
+                                '                <div class="pinglunneir"><label class="beizhu_pl">[回复'+pl_user+']</label>'+plhf+'</div>\n' +
+                                '                <div class="pl_status">\n' +
+                                '                    <label class="time_pl">1分钟前</label>\n' +
+                                '                    <label class="ico_plbtn">\n' +
+                                '                        <span class="jubao_btn">举报</span>\n' +
+                                '                        <span class="huifu_btn">回复</span>\n' +
+                                '                    </label>\n' +
+                                '                </div>\n' +
+                                '            </div>';
+
+                            $(".text_area_pl1").hide();
+                            $(".fabupjbtn1").hide();
+                            _this.closest(".pinglun_item").after(html);
                             hint("success","回复评论成功");
-                            location=location;
+                            // location=location;
 
                         }else{
                             hint("error",data.content);
